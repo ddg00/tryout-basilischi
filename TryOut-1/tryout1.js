de80eb2b6e93c14f5e15f087fe8e3bc71b253574
AryaUP <arya.ddg00@gmail.com>
2017-03-25 12:37:53 +0800

    tryout1 done

--- TryOut-1/tryout1.js
+++ TryOut-1/tryout1.js
@@ -1,21 +1,22 @@
-
 if (process.argv.length < 3) {
   console.log('Usage: node ' + process.argv[1] + ' FILENAME');
   process.exit(1);
 }
-
+// get file using fs and initiate filename by get file name from commandline
 const fs = require('fs')
   , filename = process.argv[2];
 
 fs.readFile(filename, 'utf8', function(err, data) {
+  //throw err
   if (err) throw err;
+  //inisiate vars
+  const text = data; //assign string from file to variable text
+  const words = text.match(/\w+/g); //make array base on regex
+  let numbers = []; //array of numbers
+  let uniqWords = 0; //count for uniqWords
 
-  const text = data;
-  const words = text.match(/\w+/g);
-  let numbers = [];
-  let uniqWords = 0;
-
-  var counts = words.reduce(function ( stats, word ) {
+  //start to cuunt
+  const counts = words.reduce(function ( stats, word ) {
 
     if ( stats.hasOwnProperty( word ) ) {
         stats[ word ] = stats[ word ] + 1;
@@ -25,18 +26,24 @@ fs.readFile(filename, 'utf8', function(err, data) {
         uniqWords++;
     }
 
+    //check and count if word equal number
     if(!isNaN(word)){
-      numbers.push(word);
+      numbers.push(Number(word)); // push number to numbers array
     }
 
     return stats;
 
   }, {} );
 
+  //sum numbers 
+  let totalNumbers = numbers.reduce(function (a, b) {
+      return a + b;
+  }, 0);
+
   console.log("Jumlah semua kata: "+ words.length);
   console.log("Jumlah kata yang unik: "+ uniqWords);
   console.log("Jumlah kata yang unik dan jumlahnya masing-masing: ");
   console.log(counts);
-  console.log(numbers);
-
+  console.log("Ada "+numbers.length+" angka: "+numbers.join());
+  console.log("Jumlah semua angka: "+totalNumbers);
 });
